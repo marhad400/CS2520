a32751cbbdac471ca4185160f156f5aad6f3e314
marhad400 <markhaddad03@gmail.com>
2023-04-11 15:41:35 -0700

    update house.py

--- Week11/house.py
+++ Week11/house.py
@@ -25,4 +25,46 @@ def draw_house(x, y, width, height):
 	draw_house_walls(x, y - foundation_height, walls_width, walls_height)
 	draw_house_roof(x, y - foundation_height - walls_height, width, roof_height)
 
+def draw_house_foundation(x, y, width, height):
+	"""
+	function draws house with width and height from the reference point
+
+	param x: x coorindate of the midpoint of the foundation
+	param y: y coordinate of the bottom side of the foundation
+	param width: total width of the foundation
+	param height: total heght of the foundation
+	
+	"""
+	print("Drawing foundation", x, y, width, height)
+
+	pass
+
+def draw_house_walls(x, y, width, height):
+	"""
+	function draws house with width and height from the reference point
+
+	param x: x coorindate of the midpoint of the foundation
+	param y: y coordinate of the bottom side of the foundation
+	param width: total width of the walls
+	param height: total heght of the walls
+	
+	"""
+	print("Drawing walls", x, y, width, height)
+
+	pass
+
+def draw_house_roof(x, y, width, height):
+	"""
+	function draws house with width and height from the reference point
+
+	param x: x coorindate of the midpoint of the foundation
+	param y: y coordinate of the bottom side of the foundation
+	param width: total width of the roof
+	param height: total heght of the roof
+	
+	"""
+	print("Drawing roof", x, y, width, height)
+
+	pass
+
 main()
